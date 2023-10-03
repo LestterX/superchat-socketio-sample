fc22f85a0b41d708915d644e0be9669849fec488
LestterX <lgs.alves@hotmail.com>
2023-10-03 09:31:59 -0300

    feat: starting socketio

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -6,10 +6,11 @@ import 'dotenv/config'
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(routes)
 
 const serverHTTP = createServer(app)
+import './WebSocket/HomeWS'
 
 export { serverHTTP }
